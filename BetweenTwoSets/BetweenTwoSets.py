f0ed72177a921094c9a2795227c9738a4d55290b
Caio Mateus <cmmcardoso@gmail.com>
2021-10-10 14:24:05 -0300

    missing 'f' in import

--- BetweenTwoSets/BetweenTwoSets.py
+++ BetweenTwoSets/BetweenTwoSets.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-rom copy import copy
+from copy import copy
 def lcm(a):
     """
     Function that calculates the least common multiple
